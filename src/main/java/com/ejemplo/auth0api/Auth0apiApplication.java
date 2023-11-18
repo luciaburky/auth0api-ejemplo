9cf10207d6ba83291c0aeb6c51aab9405ffed2fe
luciaburky <luciaburky1@gmail.com>
2023-11-18 19:49:24 -0300

    Initial commit

---
+++ src/main/java/com/ejemplo/auth0api/Auth0apiApplication.java
@@ -0,0 +1,13 @@
+package com.ejemplo.auth0api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Auth0apiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Auth0apiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ejemplo/auth0api/Auth0apiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.auth0api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Auth0apiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
